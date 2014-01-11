a26c0fcdd9968b42661c3b8484175844f915884e
Dlaha <dave.konec@gmail.com>
2014-01-11 09:44:45 +0100

    matchmaking

--- Othello/Othello/Controllers/HomeController.cs
+++ Othello/Othello/Controllers/HomeController.cs
@@ -9,7 +9,15 @@ namespace Othello.Controllers
 {
     public class HomeController : Controller
     {
-        private DataContext data = new DataContext();
+        private static DataContext data = new DataContext();
+
+        //private static int t = 0;
+
+        //public HomeController()
+        //    : base()
+        //{
+        //    t++;
+        //}
 
         public ActionResult Index()
         {
@@ -20,8 +28,56 @@ namespace Othello.Controllers
         public ActionResult StartGame(Player newPlayer)
         {
             data.Players.Add(newPlayer);
-            data.SaveChanges();
-            return View("WaitForPlaymate", newPlayer);
+            return CheckForPlaymate(newPlayer);
+        }
+
+        private Player findPlaymate(Player actPlayer)
+        {
+            IQueryable<Player> players = data.Players.Where(p => p.State == PlayerState.Waiting && p.Id != actPlayer.Id).OrderBy(p => p.LastUpdate);
+            DateTime stamp = DateTime.UtcNow;
+            bool ch = false; // detect change
+            Player result = null;
+            try
+            {
+                foreach (Player p in players)
+                    if ((stamp - p.LastUpdate).TotalSeconds > 10) // timeout players
+                    {
+                        p.State = PlayerState.Disconnected; ch = true;
+                    }
+                    else result = p;
+                return result;
+            }
+            finally
+            {
+                if (ch) data.SaveChanges();
+            }
+        }
+
+        public ActionResult CheckForPlaymate(Player player)
+        {
+            player = data.Players.Find(player.Id); // refresh
+            // check if this player wasn't already selected as playmate
+            if (player.State==PlayerState.Playing)
+                return View("Game", player); 
+
+            player.LastUpdate = DateTime.UtcNow;
+            try
+            {
+                // find playmate
+                Player playmate = findPlaymate(player);
+                if (playmate != null)
+                {
+                    player.State = PlayerState.Playing;
+                    playmate.State = PlayerState.Playing;
+                    return View("Game", player);
+                }
+                else // playmate wasn't found - wait for it
+                    return View("WaitForPlaymate", player);
+            }
+            finally
+            {
+                data.SaveChanges();
+            }
         }
     }
 }
--- Othello/Othello/Models/Player.cs
+++ Othello/Othello/Models/Player.cs
@@ -6,18 +6,30 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Othello.Models
 {
+    public enum PlayerState
+    {
+        Waiting,
+        Playing,
+        Disconnected
+    }
+
     public class Player
     {
         [Key] // primary key
         public int Id { get; set; }
         [Required] // not null
-        public DateTime TimeStamp { get; set; }
+        public DateTime WaitStartTime { get; set; }
         [Required] // not null
         public string Name { get; set; }
+        [Required]
+        public PlayerState State { get; set; }
+        public DateTime LastUpdate { get; set; } // last interaction from client side
+
 
         public Player()
         {
-            TimeStamp = DateTime.Now;
+            WaitStartTime = DateTime.UtcNow;
+            LastUpdate = WaitStartTime;
         }
     }
 }
